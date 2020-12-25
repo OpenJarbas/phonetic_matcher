76d59c5ffe54b32838a376b0a4844a365cfca221
jarbasal <jarbasai@mailfence.com>
2020-12-25 15:06:03 +0000

    readme

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='phonetic_matcher',
     version='0.0.1',
     packages=['phonetic_matcher'],
-    url='https://github.com/OpenJarbas/phonetic_matcher',
+    url='https://github.com/OpenJarbas/phonetic_matcherpyth',
     license='Apache-2.0',
     author='jarbasAi',
     author_email='jarbasai@mailfence.com',
