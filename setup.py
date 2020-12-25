639134835702f732e7227cad0884d4dbd8ce44ed
jarbasal <jarbasai@mailfence.com>
2020-12-25 18:05:35 +0000

    add requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
     name='phonetic_matcher',
-    version='0.1.0',
+    version='0.1.1',
     packages=['phonetic_matcher'],
     url='https://github.com/OpenJarbas/phonetic_matcherpyth',
     license='Apache-2.0',
     author='jarbasAi',
     author_email='jarbasai@mailfence.com',
+    install_requires=["phonetics",
+                      "phoneme_guesser"],
     description='matching strings by phonetics'
 )
